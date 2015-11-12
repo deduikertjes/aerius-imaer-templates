f1054b4f3dd3c68e3525d81a2cbd4c29b8bc35cc
Hilbrand Bouwkamp <hilbrand@h72.nl>
2015-11-12 15:33:04 +0100

    Added apache license.

--- SRM2Road/run_me.py
+++ SRM2Road/run_me.py
@@ -1,3 +1,19 @@
+"""
+Copyright [Participants AERIUS Masterclass: Marco Duiker, Peer vd Sande, Alex Bouthoorn, Laurens vd Burgt, Rik Zegers, Lotte Dijk, Kaj Fabri, Esther Kokmeyer, Christa Blokhuis, Anneke Donkersloot, Japer Harbers, Roy Laurijsse, Luc Meegens, Marike Aalbers]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+"""
+
 import csv
 from gml_templating_lib import render_gml_from_IMAERtemplate, IMAERgeometry
 
